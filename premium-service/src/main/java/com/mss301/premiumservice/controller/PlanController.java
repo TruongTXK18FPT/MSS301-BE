4cd482f445e8e05412bae6f3ea477512ab3a7246
datndse171888 <datndse171888@fpt.edu.vn>
2025-10-31 12:59:58 +0700

    fix api

--- premium-service/src/main/java/com/mss301/premiumservice/controller/PlanController.java
+++ premium-service/src/main/java/com/mss301/premiumservice/controller/PlanController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/plans")
+@RequestMapping("/api/v1/plans")
 @CrossOrigin
 public class PlanController {
 
